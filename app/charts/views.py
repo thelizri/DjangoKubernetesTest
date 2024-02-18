a8e9cf96ad317ba3503ea9c0d243509d54b62c78
Karlan <wcar@kth.se>
2024-02-18 20:40:08 +0100

    Add, Update, Push

--- app/charts/views.py
+++ app/charts/views.py
@@ -2,11 +2,35 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import SensorData
 from .serializers import SensorDataSerializer
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
 
 # Create your views here.
 def index(request):
-    return render(request, "charts/base.html")
+    # Fetch sensor data
+    queryset = SensorData.objects.all().order_by("time")
+
+    # Prepare data for the chart
+    time_labels = [data.time.strftime("%Y-%m-%d %H:%M:%S") for data in queryset]
+    temperatures = [data.temperature for data in queryset]
+    humidity = [data.humidity for data in queryset]
+    light = [data.light for data in queryset]
+
+    # Serialize data to JSON format
+    time_labels_json = json.dumps(time_labels, cls=DjangoJSONEncoder)
+    temperatures_json = json.dumps(temperatures, cls=DjangoJSONEncoder)
+    humidity_json = json.dumps(humidity, cls=DjangoJSONEncoder)
+    light_json = json.dumps(light, cls=DjangoJSONEncoder)
+
+    # Pass data to the template
+    context = {
+        "time_labels": time_labels_json,
+        "temperatures": temperatures_json,
+        "humidity": humidity_json,
+        "light": light_json,
+    }
+    return render(request, "charts/index.html", context)
 
 
 class SensorDataViewSet(viewsets.ModelViewSet):
