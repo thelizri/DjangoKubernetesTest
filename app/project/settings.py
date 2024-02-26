adbc063f7f4a35dec1d3b15af1e79605964bc240
Karlan <wcar@kth.se>
2024-02-26 23:47:06 +0100

    Add, Update, Push

--- app/project/settings.py
+++ app/project/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
